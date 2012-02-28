cd83959be9c5cb09322d3a686a069320772ffad1
faguirre <faguirre@faguirre.(none)>
2012-02-28 00:05:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Andesconexion::Application.config.secret_token = '4e4eca0f10efb31410269ba21ee3a786d1c784529825b6afe12c49288728c36e58d012883e10a2c31c7209654f172a58ff1871338ce33ab7981bbd9cb117a5c6'
