d60b5607e7d14d531ada07dd6e95054c4041d1ec
faguirre <faguirre@faguirre.(none)>
2012-03-09 22:59:37 -0500

    styles

--- app/controllers/admin/travel_packages_controller.rb
+++ app/controllers/admin/travel_packages_controller.rb
@@ -2,4 +2,7 @@ class Admin::TravelPackagesController < Admin::BaseController
   def index
     @travel_packages = TravelPackage.find(:all)
   end
+  def new
+    @travel_package = TravelPackage.new
+  end
 end
