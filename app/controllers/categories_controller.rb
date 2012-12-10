68502b3d1dd7db5515d08811d7d771feb76d7478
Fabian Aguirre <faguirre@nettheory.com>
2012-12-10 16:05:04 -0600

    Update app/controllers/categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,9 +6,11 @@ class CategoriesController < ApplicationController
   def show_info
     @category = Category.find(params[:main_category])
     @page_title = @category.page_title
+    @meta_content = @category.meta_tag
   end
   def show_info_sub_category
     @category = Category.find(params[:sub_category])
     @page_title = @category.page_title
+    @meta_content = @category.meta_tag
   end
 end
