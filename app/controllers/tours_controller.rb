f35343f8f03e9099b6707edc41137f3b619bafe9
Fabian Aguirre <fabianraf@gmail.com>
2014-02-01 10:27:17 -0500

    Removed impressionist

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -3,7 +3,7 @@ class ToursController < ApplicationController
     @tour = Category.find_using_slug(params[:sub_category]).tours.find_using_slug(params[:tour])
     @page_title = @tour.page_title
     @meta_content = @tour.meta_tag
-    impressionist(@tour)
+#    impressionist(@tour)
   end
   
   
